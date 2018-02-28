088d94805895ef659d65d9d029649ecec7d59cfc
Free-bird-ting <1456164221@qq.com>
2018-02-28 23:45:44 +0800

    组件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,35 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ProductComponent } from './product/product.component';
+import { StartComponent } from './start/start.component';
 
 @NgModule({
+  // 声明组件 管道 指令
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SearchComponent,
+    CarouselComponent,
+    ProductComponent,
+    StartComponent
   ],
   imports: [
+    // 浏览器
     BrowserModule,
+    // 表单
     FormsModule,
+    //
     HttpModule
   ],
+  // 模块用的什么服务
   providers: [],
+  // 模块的主组件
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
+// 使用什么模块来启动服务
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// 关闭开发者模式
 import { enableProdMode } from '@angular/core';
+// 环境配置
 import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
 
